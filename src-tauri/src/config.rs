b942b046c16e4914159eb13133ed7609a6e4c21f
Chandan Kumar Mandal <chandan_kumarmandal@apple.com>
2025-07-06 09:56:21 +0530

    config: update config to store default store settings
    Signed-off-by: Chandan Kumar Mandal <chandan_kumarmandal@apple.com>

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -6,17 +6,22 @@ use std::path::PathBuf;
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct AppConfig {
   pub db_mode: Option<String>,
+  pub mongodb_uri: Option<String>,
+  pub sqlite_file: Option<String>,
 }
 
 impl AppConfig {
   pub fn load() -> Self {
     let path = Self::config_file_path();
-    if path.exists() {
+    let config = if path.exists() {
       let content = fs::read_to_string(&path).unwrap();
-      toml::from_str(&content).unwrap()
+      toml::from_str(&content).unwrap_or_else(|_| AppConfig::default())
     } else {
-      Self { db_mode: None }
-    }
+      AppConfig::default()
+    };
+
+    config.save();
+    config
   }
 
   pub fn save(&self) {
@@ -31,5 +36,22 @@ impl AppConfig {
     fs::create_dir_all(&dir).unwrap();
     dir.push("config.toml");
     dir
+  }
+
+  fn default_mongodb_uri() -> String {
+    "mongodb+srv://armorixdb_rw:xXHB7NoDzhrgccND@cluster0.cblkv.mongodb.net/armorixdb?retryWrites=true&w=majority"
+      .to_string()
+  }
+
+  fn default_sqlite_file() -> String {
+    "armorix.db".to_string()
+  }
+
+  fn default() -> Self {
+    AppConfig {
+      db_mode: None,
+      mongodb_uri: Some(Self::default_mongodb_uri()),
+      sqlite_file: Some(Self::default_sqlite_file()),
+    }
   }
 }
